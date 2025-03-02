2fed45a4bea50a504c3c7712d50c0bcd9fd70fb1
dgurjar <dgurjar@adobe.com>
2025-03-02 18:30:29 +0530

    fix gh-pages rendition

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material';
 import { CartProvider } from './context/CartContext';
 import { AuthProvider } from './context/AuthContext';
